19c1ad0441bc03252c2b42f1c791d6cff66729e2
Shaneil Clarke <shaneilske@yahoo.com>
2018-12-12 15:55:19 +0000

    separated form into separate file

--- app.rb
+++ app.rb
@@ -1,13 +1,16 @@
 require 'sinatra'
 
+get '/' do
+  'Hello'
+end
+
 get '/random_cat' do
   @name = ["Amigo", "Oscar", "Viking"].shuffle.pop
   erb(:index)
 end
 
-get '/named_cat' do
-  @name = params[:name]
-  erb(:index)
+get '/form' do
+  erb :form
 end
 
 post '/named_cat' do
